ab696063feb2763d1325fe5509dc32346acebde4
Eleni Voreopoulou <evoreopoulou@skywalker.gr>
2019-09-09 16:13:41 +0300

    Updated Login Button to be enabled only if fields are filled in

--- app/src/main/java/com/sky/casper/skywalker_new_app/Activities/ActivityMainLogin.java
+++ app/src/main/java/com/sky/casper/skywalker_new_app/Activities/ActivityMainLogin.java
@@ -5,11 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import com.sky.casper.skywalker_new_app.R;
 
+import org.w3c.dom.Text;
+
 public class ActivityMainLogin extends AppCompatActivity {
+
+    /*Variables for if username and password not filled in login button is disabled*/
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private Button buttonConfirm;
 
     /*Variables for background animation*/
     RelativeLayout relativeLayout;
@@ -33,7 +44,35 @@ public class ActivityMainLogin extends AppCompatActivity {
         animationDrawable.start();
         /*Start Background Animation*/
 
+        /*Check if fields username and password are filled in */
+        editTextUsername = findViewById(R.id.edit_text_username);
+        editTextUsername = findViewById(R.id.edit_text_password);
+        buttonConfirm = findViewById(R.id.button_confirm);
+
+        editTextUsername.addTextChangedListener(loginTextWatcher);
+        editTextPassword.addTextChangedListener(loginTextWatcher);
+
        }
+       private TextWatcher loginTextWatcher = new TextWatcher() {
+           @Override
+           public void beforeTextChanged(CharSequence charSequence, int start, int count, int after) {
+
+           }
+
+           @Override
+           public void onTextChanged(CharSequence charSequence, int start, int count, int after) {
+               String usernameInput = editTextUsername.getText().toString().trim();
+               String passwordInput = editTextPassword.getText().toString().trim();
+
+               buttonConfirm.setEnabled(!usernameInput.isEmpty()&& !passwordInput.isEmpty());
+
+           }
+
+           @Override
+           public void afterTextChanged(Editable editable) {
+
+           }
+       };
 
     public void btn_signup(View view) {
         startActivity(new Intent(getApplicationContext(),ActivitySignUp.class));
