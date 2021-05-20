77e3e745a349b71cd7131c97cc5d270b913fe81e
Eleni Voreopoulou <evoreopoulou@skywalker.gr>
2021-05-20 10:18:02 +0300

    Initial Commit

--- app/src/main/java/com/sky/casper/skywalker_new_app/Models/EduType.java
+++
@@ -1,8 +0,0 @@
-package com.sky.casper.skywalker_new_app.Models;
-
-
-public class EduType extends Type {
-    public EduType(String i, String t, String ty) {
-        super(i, t, ty);
-    }
-}
--- app/src/main/java/com/sky/casper/skywalker_new_app/Models/ForeignLang.java
+++
@@ -1,9 +0,0 @@
-package com.sky.casper.skywalker_new_app.Models;
-
-
-public class ForeignLang extends Type {
-
-    public ForeignLang(String i, String t, String ty) {
-        super(i, t, ty);
-    }
-}
